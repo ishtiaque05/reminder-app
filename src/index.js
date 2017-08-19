1381e0c6d3b8f998d24c422e4c67ef21915719d0
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2017-08-19 14:49:24 +0600

    added logic for removing list using redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducers';
+import './index.css'
 
-const store = createStore();
+const store = createStore(reducer);
 ReactDOM.render(
   <Provider store={store}>
   <App/>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 const reminder = (action) => {
   return {
@@ -6,11 +6,27 @@ const reminder = (action) => {
     id: Math.random()
   }
 }
+
+const removeById = (state=[], id) => {
+  const reminders = state.filter(reminder => reminder.id != id);
+  console.log('new reduced reminders', reminders);
+  return reminders;
+}
+
 const reminders = (state = [], action) =>{
+  console.log('this is stateeeeeeeeee', state)
   let reminders = null;
   switch(action.type){
     case ADD_REMINDER:
       reminders = [...state, reminder(action)];
-      console.log('reminders as s')
+      console.log('reminders as state', reminders);
+      return reminders;
+    case DELETE_REMINDER:
+       reminders = removeById(state, action.id);
+       return reminders;
+    default:
+      return state;
   }
 }
+
+export default reminders;
